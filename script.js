21679b148e36fdc66020d59067fbc3234c068184
DixonMch <dixonmogaka@gmail.com>
2023-03-14 00:15:29 +0300

    Simple website

---
+++ script.js
@@ -0,0 +1,6 @@
+function handleClick () {
+    alert("Thank you for checking my details.")
+}
+const h2 = document.createElement("h2");
+    h2.textContent = "Code with me.";
+    document.querySelector("body").appendChild(h2);
